e96ffd654cbf2450b5c6b00be565b418628da436
sillah2010 <macktastic2014@gmail.com>
2017-11-01 12:35:29 -0400

    Splash page styled again

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,22 @@ const SplashPage = styled.div`
   text-align: center;
   font-family: 'Cabin Condensed', sans-serif;
   font-size: 30px;
-  img {
-    width: 100%
+  img, .title {
+    width: 100%;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   }
 
- 
+  .title {
+    background-color: #bddfeb;
+    padding: 30px 0px 30px
+  }
 `
 
 class App extends Component {
   render() {
     return (
       <SplashPage>
-        <h1>Project Vagabond: </h1>
+        <h1 className="title">Project Vagabond: </h1>
         <img src="http://passportinfoguide.com/wp-content/uploads/2013/11/passport.jpg" alt="passport"/>
       </SplashPage>
 
