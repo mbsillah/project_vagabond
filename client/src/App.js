2263f57e3ee3a77cb3ede225baae1165ac44cf0e
sillah2010 <macktastic2014@gmail.com>
2017-11-01 12:17:44 -0400

    Splash page styled

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 
+const SplashPage = styled.div`
+  text-align: center;
+  font-family: 'Cabin Condensed', sans-serif;
+  font-size: 30px;
+  img {
+    width: 100%
+  }
+
+ 
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Project Vagabond</h1>
-      </div>
+      <SplashPage>
+        <h1>Project Vagabond: </h1>
+        <img src="http://passportinfoguide.com/wp-content/uploads/2013/11/passport.jpg" alt="passport"/>
+      </SplashPage>
+
     );
   }
 }
