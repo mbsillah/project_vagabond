0e3f311b6ef44381cf6075f0c61eb47253aa54f7
ebhinch <erica.hinchman@gmail.com>
2017-11-01 14:31:14 -0400

    set background image

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,15 @@ import styled from 'styled-components'
 const SplashPage = styled.div`
   text-align: center;
   font-family: 'Cabin Condensed', sans-serif;
-  font-size: 30px;
-  img, .title {
+  font-size: 20px;
+
+.title {
     width: 100%;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  }
-
-  .title {
     background-color: #bddfeb;
-    padding: 30px 0px 30px
+    opacity: .8;
+    padding: 20px 0px 20px 0px;
+    letter-spacing: 5px;
   }
 `
 
@@ -20,8 +20,8 @@ class App extends Component {
   render() {
     return (
       <SplashPage>
-        <h1 className="title">Project Vagabond: </h1>
-        <img src="http://passportinfoguide.com/wp-content/uploads/2013/11/passport.jpg" alt="passport"/>
+        <h1 className="title">Project Vagabond </h1>
+
       </SplashPage>
 
     );
