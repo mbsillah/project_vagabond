bf87bbf293013d15f1981da8a84b4be866e3f0ba
ebhinch <erica.hinchman@gmail.com>
2017-11-02 11:28:34 -0400

    built out routes for post model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do 
-    resources :cities
+    resources :cities do 
+      resources :posts
+    end
     resources :users
   end
 
