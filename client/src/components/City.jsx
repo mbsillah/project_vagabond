0ac776ffcfbd5b8d646567d8045f5cacb816d75c
sillah2010 <macktastic2014@gmail.com>
2017-11-02 17:00:35 -0400

    Did every route for post except edit route

--- client/src/components/City.jsx
+++ client/src/components/City.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import NewPost from './NewPost'
 import styled from "styled-components"
 
 const CityName = styled.div `
@@ -22,28 +23,62 @@ font-family: 'Cabin Condensed', sans-serif;
 class City extends Component {
 
     state = {
-        city: {}
+        city: {},
+        posts: [],
+        showForm: false
     }
 
 
+
+
 async componentWillMount(){
+  this.getCitiesAndPosts();
+
+}
+
+async componentWillReceiveProps(){
+    if (this){}
+}
+
+pushPosts = (newPost) => {
+    const newArray = [...this.state.posts]
+    newArray.push(newPost)
+    this.setState({posts: newArray})
+}
+
+getCitiesAndPosts = async () => {
     try {
         const { id } = this.props.match.params
         const response = await axios.get(`/api/cities/${id}`)
-
         this.setState({city: response.data})
-
+        const postResponse = await axios.get(`/api/cities/${id}/posts`)
+        this.setState({posts: postResponse.data})
 
     } catch(error) {
         console.log(error)
     }
 }
 
+
+deletePost = async (post) => {
+   try {
+       const { id } = this.props.match.params    
+       const response = await axios.delete(`/api/cities/${id}/posts/${post}`)
+       console.log(response)
+       this.setState({posts: response.data}) 
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+toggleShowForm = () => {
+    this.setState({showForm: !this.state.showForm})
+}
+
 // const population = {this.state.city.population}
 
     render() {
 
-    
 
         return (
             <div>
@@ -52,8 +87,17 @@ async componentWillMount(){
                 <div>Population: {this.state.city.population}</div>
                 <div>About: {this.state.city.description}</div>
                 <img src={this.state.city.photo_url} />
-
                 </CityDetails>
+                {this.state.posts.map(post => (
+                    <div key={post.id}>
+                        <h4>{post.title}</h4>
+                        <h5>{post.text}</h5>
+                        <button onClick={() => this.deletePost(post.id)}>Delete Post</button>
+                        <button>Edit</button>
+                    </div>
+                ))}
+                <button onClick={this.toggleShowForm}>Add New Post</button>
+                {this.state.showForm ? <NewPost pushPosts={this.pushPosts} id={this.props.match.params.id} handleSubmit={this.handleSubmit} /> : null}
             </div>
         );
     }
