64fba061c12ab24fd57685eba66dfdf2b6131460
ebhinch <erica.hinchman@gmail.com>
2017-11-02 16:02:12 -0400

    added in polaroid styling to citylist

--- client/src/components/CityList.jsx
+++ client/src/components/CityList.jsx
@@ -10,15 +10,27 @@ const CityContainer = FlexRow.extend`
     margin-left: 15px;
     margin-right: 15px;
     font-family: 'Rammetto One', cursive;
+    opacity: .9;
+
 `
 
 const CityCard = styled.div`
     display: flex;
     flex-direction: column;
-    width: 30%;
-    font-size: 30px;
+    // width: 30%;
+    font-size: 20px;
+        width: 300px;  height: 300px; margin: 40px auto;
+
     text-align: center;
     justify-content: space-around;
+    float: left;
+    padding: 20px 10px 15px 10px;
+    background: #eee;
+    border: 1px solid #fff;
+    -moz-box-shadow: 0px 2px 15px #333;
+    position: relative;
+    -webkit-transform: rotate(-8deg);
+    -moz-transform: rotate(-8deg);
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     background-color: white;
     margin-left: 10px;
@@ -32,6 +44,9 @@ const CityCard = styled.div`
         padding-bottom: 10px;
     }
 `
+
+
+
 
 const CityList = (props) => {
     return (
