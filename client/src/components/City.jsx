fc3c257afb75119b2a9d16c9ab9d43aa1573bbcd
sillah2010 <macktastic2014@gmail.com>
2017-11-03 10:09:10 -0400

    Finished edit route and fixed static new post page

--- client/src/components/City.jsx
+++ client/src/components/City.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import axios from "axios";
 import NewPost from './NewPost'
 import styled from "styled-components"
+import { Link } from 'react-router-dom'
 
-const CityImageBanner = styled.div `
+const CityImageBanner = styled.div`
 
     img {
         width: 100%;
@@ -19,7 +20,7 @@ const CityImageBanner = styled.div `
     }
   `
 
-  const CityText = styled.div`
+const CityText = styled.div`
     width: 100%;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0   6px 20px 0 rgba(0, 0, 0, 0.19);
     padding-top: 0px;
@@ -31,7 +32,7 @@ const CityImageBanner = styled.div `
 
   `
 
-const CityName = styled.div `
+const CityName = styled.div`
     font-family: 'Rammetto One', cursive;
     font-size: 38px;
     text-align: center;
@@ -39,7 +40,7 @@ const CityName = styled.div `
     padding-top: 25px;
 `
 
-const CityBody = styled.div `
+const CityBody = styled.div`
       box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
           font-family: 'Cabin Condensed', sans-serif;
     font-size: 30px;
@@ -60,77 +61,76 @@ class City extends Component {
 
 
 
-async componentWillMount(){
-  this.getCitiesAndPosts();
+    async componentWillMount() {
+        this.getCitiesAndPosts();
 
-}
-
-async componentWillReceiveProps(){
-    if (this){}
-}
-
-pushPosts = (newPost) => {
-    const newArray = [...this.state.posts]
-    newArray.push(newPost)
-    this.setState({posts: newArray})
-}
-
-getCitiesAndPosts = async () => {
-    try {
-        const { id } = this.props.match.params
-        const response = await axios.get(`/api/cities/${id}`)
-        this.setState({city: response.data})
-        const postResponse = await axios.get(`/api/cities/${id}/posts`)
-        this.setState({posts: postResponse.data})
-
-    } catch(error) {
-        console.log(error)
     }
-}
 
+    getCitiesAndPosts = async () => {
+        try {
+            const { id } = this.props.match.params
+            const response = await axios.get(`/api/cities/${id}`)
+            this.setState({ city: response.data })
+            const postResponse = await axios.get(`/api/cities/${id}/posts`)
+            this.setState({ posts: postResponse.data })
 
-deletePost = async (post) => {
-   try {
-       const { id } = this.props.match.params    
-       const response = await axios.delete(`/api/cities/${id}/posts/${post}`)
-       console.log(response)
-       this.setState({posts: response.data}) 
-    } catch (error) {
-        console.log(error)
+        } catch (error) {
+            console.log(error)
+        }
     }
-}
 
-toggleShowForm = () => {
-    this.setState({showForm: !this.state.showForm})
-}
 
-// const population = {this.state.city.population}
+    deletePost = async (post) => {
+        try {
+            const { id } = this.props.match.params
+            const response = await axios.delete(`/api/cities/${id}/posts/${post}`)
+            console.log(response)
+            this.setState({ posts: response.data })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    toggleShowForm = () => {
+        this.setState({ showForm: !this.state.showForm })
+    }
+
+    pushPosts = (newPost) => {
+        const newArray = [...this.state.posts]
+        newArray.push(newPost)
+        this.setState({ posts: newArray })
+        this.toggleShowForm()
+    }
+
+    // const population = {this.state.city.population}
 
     render() {
 
 
         return (
             <div>
-            <CityImageBanner><img src={this.state.city.photo_url} /></CityImageBanner>
-            <CityBody>
-                <CityText>
-                <CityName>Welcome to {this.state.city.name}</CityName>
-                <div>Population: {this.state.city.population}</div>
-                <div>About: {this.state.city.description}</div>
-                <img src={this.state.city.photo_url} />
-              
-                {this.state.posts.map(post => (
-                    <div key={post.id}>
-                        <h4>{post.title}</h4>
-                        <h5>{post.text}</h5>
-                        <button onClick={() => this.deletePost(post.id)}>Delete Post</button>
-                        <button>Edit</button>
-                    </div>
-                ))}
-                <button onClick={this.toggleShowForm}>Add New Post</button>
-                {this.state.showForm ? <NewPost pushPosts={this.pushPosts} id={this.props.match.params.id} handleSubmit={this.handleSubmit} /> : null}
-                </CityText>
-            </CityBody>
+                <CityImageBanner><img src={this.state.city.photo_url} /></CityImageBanner>
+                <CityBody>
+                    <CityText>
+                        <CityName>Welcome to {this.state.city.name}</CityName>
+                        <div>Population: {this.state.city.population}</div>
+                        <div>About: {this.state.city.description}</div>
+                        <img src={this.state.city.photo_url} />
+
+                        {this.state.posts.map(post => (
+                            <div key={post.id}>
+                                <h4>{post.title}</h4>
+                                <h5>{post.text}</h5>
+                                <button onClick={() => this.deletePost(post.id)}>Delete Post</button>
+                                <Link to={`/cities/${this.state.city.id}/posts/${post.id}`}>
+                                <button>Edit</button>
+                                </Link>
+                            </div>
+                        ))}
+                        <button onClick={this.toggleShowForm}>Add New Post</button>
+                        {this.state.showForm ? <NewPost pushPosts={this.pushPosts} id={this.props.match.params.id} toggleShowForm={this.toggleShowForm}/> : null}
+                    </CityText>
+                </CityBody>
             </div>
         );
     }
---
+++ client/src/components/EditPost.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
+
+class EditPost extends Component {
+  
+              
+    
+    
+    state = {
+        post: {
+        title: "",
+        text: ""
+        },
+        redirectToCityPage: false
+        
+    }
+
+    handleChange = (event) => {
+        const attribute = event.target.name
+        const updatePost = { ...this.state.post }
+        updatePost[attribute] = event.target.value
+        this.setState({ post: updatePost })
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault()
+        const cityId = this.props.match.params.city_id
+        const postId = this.props.match.params.id
+        const response = await axios.patch(`/api/cities/${cityId}/posts/${postId}`, {
+            post: this.state.post
+        })
+        this.setState({redirectToCityPage: true, post: response.data})        
+        
+    }
+
+
+
+    render() {
+
+        if (this.state.redirectToCityPage) {
+            const cityId = this.props.match.params.city_id
+            return <Redirect to={`/ cities/${cityId}`} />
+        }
+
+        return (
+            <div>
+                <h1>Edit Current Post</h1>
+              <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor="title">Title: </label>
+                        <input onChange={this.handleChange} name="title" type="text" value={this.state.post.title} />
+                    </div>
+                    <div>
+                        <label htmlFor="text">Text: </label>
+                        <textarea onChange={this.handleChange} name="text" type="text" value={this.state.post.text} />
+                    </div>
+                    <button>Save Post</button>
+                    </form>
+            </div>
+        );
+    }
+}
+
+
+export default EditPost;
