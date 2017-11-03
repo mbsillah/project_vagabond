f2ce4537d43a6d1a039dab6752fc620132f2b7bc
ebhinch <erica.hinchman@gmail.com>
2017-11-03 12:07:17 -0400

    added confirmation sentence to confirmation alert

--- client/src/components/ConfirmationAlert.jsx
+++ client/src/components/ConfirmationAlert.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const ConfirmationAlert = (props) => {
     return (
         <div>
+            <h4>Are you sure you want to delete this post? </h4>
             <button onClick={() => props.deletePost(props.id)}>Confirm </button>
             <button onClick={() => props.toggleShowConfirmationAlert()}>Cancel</button>
         </div>
